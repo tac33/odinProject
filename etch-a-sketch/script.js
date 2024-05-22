929fb4bcb5379ebdf5d697e5471fe0b9937db65a
tac33 <collins.tyrese99@gmail.com>
2024-05-22 14:34:29 -0400

    Created Grid, on hover colors

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,18 +1,40 @@
 
+function main() {
+    createDiv();
+
+    let boxes = document.getElementsByClassName("box");
+
+    for (let i=0; i < boxes.length; i++){
+        boxes[i].addEventListener("mouseover", e => {
+            boxes[i].style.backgroundColor = "blue"
+        }, false)
+    }
+}
+
 function createDiv(){
-    const newDiv = document.createElement('div');
     const current = document.querySelector(".grid-container");
     const myStyle = getComputedStyle(current);
     const height = myStyle.height;
 
-    const heighINT = Number(height);
+    let gridRow = 16;
+    let gridCol = 16;
 
-    newDiv.style.backgroundColor = "red";
-    newDiv.style.height = '10px';
-    newDiv.style.width = '10px';
+    for (let i = 0; i < gridCol; i++) {
+        const container = document.createElement('div');
+        container.style.display = "flex";
+        current.appendChild(container);
 
-    console.log(height + heighINT);
-    current.appendChild(newDiv);
+        for (let i = 0; i < gridRow; i++){
+            const newDiv = document.createElement('div');
+            newDiv.style.backgroundColor = "white";
+            newDiv.style.width = "100%";
+            newDiv.style.height = "32px";
+            newDiv.classList.add("box");
+            
+            container.appendChild(newDiv);
+        }
+    }
+    
 }
 
-createDiv();
+main();
