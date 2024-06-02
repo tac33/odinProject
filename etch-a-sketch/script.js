860c1380cd2fe55d2c028311efe9ce71fd1a96b0
tac33 <collins.tyrese99@gmail.com>
2024-06-02 19:32:18 -0400

    Basic Funtionality of Etch-A-Sketch finished.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,40 +1,97 @@
 
 function main() {
-    createDiv();
+    let slider = document.getElementById("boxSize");
+    let output = document.getElementById("demo");
+    let size = slider.value;
+    output.innerHTML = size;
 
-    let boxes = document.getElementsByClassName("box");
+    createDiv(size);
+    drawBoxes();
+    clearBoxes();
 
-    for (let i=0; i < boxes.length; i++){
-        boxes[i].addEventListener("mouseover", e => {
-            boxes[i].style.backgroundColor = "blue"
-        }, false)
+    // On slider change, change grid size.
+    slider.oninput = () => {
+        output.innerHTML = slider.value;
+        resetGrid();
+        createDiv(slider.value);
+        drawBoxes();
     }
 }
 
-function createDiv(){
+// Create div elements for grid
+function createDiv(gridRow){
     const current = document.querySelector(".grid-container");
-    const myStyle = getComputedStyle(current);
-    const height = myStyle.height;
 
-    let gridRow = 16;
-    let gridCol = 16;
+    let size = 800.0 / gridRow;
 
-    for (let i = 0; i < gridCol; i++) {
+    for (let i = 0; i < gridRow; i++) {
         const container = document.createElement('div');
         container.style.display = "flex";
+        container.style.height = size + "px";
+        container.classList.add("boxContainer");
         current.appendChild(container);
 
         for (let i = 0; i < gridRow; i++){
             const newDiv = document.createElement('div');
             newDiv.style.backgroundColor = "white";
-            newDiv.style.width = "100%";
-            newDiv.style.height = "32px";
+            newDiv.style.width = size + "px";
+            newDiv.style.height = "auto";
             newDiv.classList.add("box");
             
             container.appendChild(newDiv);
         }
     }
     
+}
+
+// reset grid to all white
+function clearBoxes(){
+    let boxes = document.getElementsByClassName("box");
+    let btn = document.getElementById("clear");
+
+    // Reset Grid
+    btn.onclick = () => {
+        for (let i=0; i < boxes.length; i++){
+            boxes[i].style.backgroundColor = "white";
+        }
+    }
+}
+
+// Draw boxes on grid
+function drawBoxes(){
+    let boxes = document.getElementsByClassName("box");
+    let eraseBtn = document.getElementById("eraser");
+    let color = document.getElementById("pickColor");
+    let eraser = false;
+
+    // Handle erase Toggle
+    eraseBtn.onclick = () => {
+        if (eraser == false){
+            eraser = true;
+        } else {
+            eraser = false;
+        }
+    }
+
+    // Draw boxes depending on erase toggle
+    for (let i=0; i < boxes.length; i++){
+        boxes[i].addEventListener("mouseover", e => {
+            if (eraser){
+                boxes[i].style.backgroundColor = "white";
+            } else {
+                boxes[i].style.backgroundColor = color.value;
+            }
+        }, false)
+    }
+}
+
+// Delete all div in Grid
+function resetGrid(){
+    let grid = document.getElementById("grid-container");
+
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild)
+    }
 }
 
 main();
