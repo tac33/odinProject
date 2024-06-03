0ebddcdb0367acd4ae364a76bdfe4a1e8acd0602
tac33 <collins.tyrese99@gmail.com>
2024-06-02 21:38:33 -0400

    Added Rainbow button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -22,7 +22,7 @@ function main() {
 function createDiv(gridRow){
     const current = document.querySelector(".grid-container");
 
-    let size = 800.0 / gridRow;
+    let size = 400 / gridRow;
 
     for (let i = 0; i < gridRow; i++) {
         const container = document.createElement('div');
@@ -59,17 +59,39 @@ function clearBoxes(){
 
 // Draw boxes on grid
 function drawBoxes(){
-    let boxes = document.getElementsByClassName("box");
-    let eraseBtn = document.getElementById("eraser");
-    let color = document.getElementById("pickColor");
+    const boxes = document.getElementsByClassName("box");
+    const eraseBtn = document.getElementById("eraser");
+    const rainbowBtn = document.getElementById("rainbow");
+    const color = document.getElementById("pickColor");
     let eraser = false;
+    let rainbow = false;
+    let counter = 0;
+    const colors = ["red", "orange", "yellow", "green", "blue", "violet"];
 
     // Handle erase Toggle
     eraseBtn.onclick = () => {
+        // Check if rainbow is on.
+        if (rainbow == true){
+            rainbow = false;
+        }
+        
         if (eraser == false){
             eraser = true;
         } else {
             eraser = false;
+        }
+    }
+
+    rainbowBtn.onclick = () => {
+        // check if eraser is on
+        if (eraser == true){
+            eraser = false;
+        }
+
+        if (rainbow == false){
+            rainbow = true;
+        } else {
+            rainbow = false;
         }
     }
 
@@ -78,9 +100,13 @@ function drawBoxes(){
         boxes[i].addEventListener("mouseover", e => {
             if (eraser){
                 boxes[i].style.backgroundColor = "white";
+            } else if (rainbow) {
+                counter = counter % 7;
+                boxes[i].style.backgroundColor = colors[counter];
+                counter += 1;
             } else {
-                boxes[i].style.backgroundColor = color.value;
-            }
+                    boxes[i].style.backgroundColor = color.value;
+                }
         }, false)
     }
 }
